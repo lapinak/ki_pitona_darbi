112e365a6f269841b845efd412b37b1304cad781
lapinak <karii.a.lap123@gmail.com>
2025-01-16 00:17:55 +0200

    regex update

--- log_file/main.py
+++ log_file/main.py
@@ -1,7 +1,7 @@
 import re
 
 faila_lokacija = ""
-regex = r'\bJan\s\d{1,2}\s\d{2}:\d{2}:\d{2}\b'
+regex = r'(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+\S+\s+\S+:\s+(?:\[\s*\d+\.\d+\]\s+)?(.+)'
 
 with open(faila_lokacija, 'r') as file:
     [print(re.findall(regex, line)) for line in file if 'USB' in line]
