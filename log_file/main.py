ab1cecd8d56ac2312000df5c734db823bde9f6d5
lapinak <karii.a.lap123@gmail.com>
2025-01-15 23:48:09 +0200

    Bez saites uz faila atrašanās vietu

--- log_file/main.py
+++ log_file/main.py
@@ -1,4 +1,4 @@
-faila_lokacija = "C:\\Users\\User\\Documents\\GitHub\\ki_pitona_darbi\\log_file\\testfile.txt"
+faila_lokacija = ""
 
 with open(faila_lokacija, 'r') as file:
     [print(line) for line in file if 'USB' in line]
