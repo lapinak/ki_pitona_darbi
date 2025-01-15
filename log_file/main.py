afdafbd9c65818d64884bd7a4c1091072e208a5b
lapinak <karii.a.lap123@gmail.com>
2025-01-15 23:56:37 +0200

    izrukā timestamp

--- log_file/main.py
+++ log_file/main.py
@@ -1,6 +1,9 @@
+import re
+
 faila_lokacija = ""
+regex = r'\bJan\s\d{1,2}\s\d{2}:\d{2}:\d{2}\b'
 
 with open(faila_lokacija, 'r') as file:
-    [print(line) for line in file if 'USB' in line]
+    [print(re.findall(regex, line)) for line in file if 'USB' in line]
 
 file.close()
