dd0e34962ec500ff8b1fb0afd159435c35cf0487
lapinak <karii.a.lap123@gmail.com>
2025-01-16 11:59:09 +0200

    saraksts

---
+++ ftp_parser/main.py
@@ -0,0 +1,13 @@
+import threading
+
+sites = [
+    "ftp://ftp.intron.ac/",
+    "ftp://ftp.ox.ac/",
+    "ftp://ftp.robelle3000.ai/",
+    "ftp://ftp.linux.org.ar/",
+    "ftp://ftp.secyt.gov.ar/",
+    "ftp://ftp.slackware.at/",
+    "ftp://ftp.bbs.com.au/",
+    "ftp://ftp.cyber.com.au/",
+    "ftp://ftp.conundrum.com/",
+]
